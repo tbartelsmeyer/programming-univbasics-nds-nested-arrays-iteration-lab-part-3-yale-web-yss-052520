6e2d2f4a81d3f3c9e5e1e56a208bf72d6f517413
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-20 05:24:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,7 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  out_string = nil
+  out_string = ""
   src.length.times do |row_i|
     src[row_i].length.times do |el_i|
       if src[row_i][el_i].is_a? String
